de444c7b0607679da409b03fda5f3cad737137bf
shkum0330 <shkum0330@gmail.com>
2024-10-31 14:38:31 +0900

    feat: mbti를 선택하지 않았을 때, ai가 mbti를 지정하도록 구현

--- src/main/java/com/eureka/kidsworld/ai/service/AiService.java
+++ src/main/java/com/eureka/kidsworld/ai/service/AiService.java
@@ -1,12 +1,14 @@
 package com.eureka.kidsworld.ai.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AiService {
     private final ChatClient chatClient;
 
@@ -21,9 +23,11 @@ public class AiService {
     }
     public String getRecommendedMbtiByBook(String title, String author) {
         String userPrompt = String.format(promptTemplate, title, author);
-        return chatClient.prompt()
+        String s= (String) chatClient.prompt()
                 .user(userPrompt)
                 .call()
                 .content();
+        log.info("추출된 mbti: {}",s);
+        return s;
     }
 }
